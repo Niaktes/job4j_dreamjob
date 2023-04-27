030128f3f2f8fd8aab7d3e7531f88cae82d31c11
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-04-27 06:31:11 +0300

    3.2.4.4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]ThreadSafe

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -1,20 +1,24 @@
 package ru.job4j.dreamjob.repository;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@ThreadSafe
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
 
-    private int nextId = 1;
-
-    private final Map<Integer, Candidate> candidates = new HashMap<>();
+    private final AtomicInteger nextId = new AtomicInteger(1);
+    @GuardedBy("this")
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     public MemoryCandidateRepository() {
         LocalDateTime dateTime = LocalDateTime.of(2023, 3, 21, 12, 0);
@@ -25,7 +29,7 @@ public class MemoryCandidateRepository implements CandidateRepository {
 
     @Override
     public Candidate save(Candidate candidate) {
-        candidate.setId(nextId++);
+        candidate.setId(nextId.getAndIncrement());
         candidates.put(candidate.getId(), candidate);
         return candidate;
     }
